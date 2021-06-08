869edd70c1b73682e6feca89b0c9db626fe464f5
masaki420 <zyab5275@gmail.com>
2021-06-09 06:57:38 +0900

    footer, dark

--- bifree-app/Docks/lara-d/resources/views/commons/footer.blade.php
+++ bifree-app/Docks/lara-d/resources/views/commons/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-	<div class="align-items-center container text-center my-3 rounded">
+	<div class="align-items-center container text-center my-3 rounded bg-dark text-white mt-5">
 		(c)Bifree
 	</div>
 </footer>
--- bifree-app/Docks/lara-d/resources/views/welcome.blade.php
+++ bifree-app/Docks/lara-d/resources/views/welcome.blade.php
@@ -1,7 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
+
     <div class="text-center">
         <h1>ようこそBifree</h1>
     </div>
+    <div class="d-flex flex-column align-items-center mt-5">
+        <div class="col-xl-7 col-lg-8 col-md-10 col-sm-11 post-card">
+            <div class="card">
+                <div class="card-header">
+                    投稿の新規作成
+                </div>
+                <div class="card-body">
+                    <form action="" class="upload">
+                        <div class="md-form">
+                            <label for="">Name</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Text</label>
+                            <textarea name="" id="" class="form-control" rows="5"></textarea>
+                        </div>
+                        <div class="text-right mt-2">
+                            <button class="btn btn-primary">投稿</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mt-5">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">Sample</th>
+                <th scope="col">Sample</th>
+                <th scope="col">Sample</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">Sample</th>
+                <td>Sample</td>
+                <td>Sample</td>
+            </tr>
+            <tr>
+                <th scope="row">Sample</th>
+                <td>Sample</td>
+                <td>Sample</td>
+            </tr>
+            <tr>
+                <th scope="row">Sample</th>
+                <td>Sample</td>
+                <td>Sample</td>
+            </tr>
+            <tr>
+                <th scope="row">Sample</th>
+                <td>Sample</td>
+                <td>Sample</td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
+    
 @endsection
