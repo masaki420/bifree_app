0acba1e95db9579879e04b04704c5ac0ac16214e
masaki420 <zyab5275@gmail.com>
2021-06-09 05:48:04 +0900

    app.blade.php

---
+++ bifree-app/Docks/lara-d/resources/views/auth/login.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+	<div class="text-center">
+		<div class="form-signin">
+			<form action="">
+				<h1 class="h3 mb-3 fw-normal">ログイン</h1>
+				<div class="form-floating">
+
+				</div>
+				<div class="form-floating">
+					
+				</div>
+			</form>
+		</div>
+	</div>
+
+@endsection
--- bifree-app/Docks/lara-d/resources/views/commons/header.blade.php
+++ bifree-app/Docks/lara-d/resources/views/commons/header.blade.php
@@ -1,8 +1,17 @@
-<header>
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+	<a href="" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+		<span class="fs-4">
+			Bifree
+		</span>
+	</a>
 	<nav>
-		<ul>
-			<li><a href="">Login</a></li>
-			<li><a href="">Signup</a></li>
+		<ul class="nav nav-pills">
+			<li class="nav-item"><a href="" class="nav-link">お友達募集</a></li>
+			<li class="nav-item"><a href="" class="nav-link">みんなのQ&A</a></li>
 		</ul>
 	</nav>
+	<div class="col-md-3 text-end">
+		<button class="btn btn-outline-primary me-2">Login</button>
+		<button class="btn btn-outline-primary me-2">SignUp</button>
+	</div>
 </header>
